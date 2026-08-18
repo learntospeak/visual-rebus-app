b07e36c5fb3552621a934201547163555c3e85a8
learntospeak <227579325+learntospeak@users.noreply.github.com>
2026-08-18 15:33:49 +1000

    Match compact keyboard to Gboard layout

--- src/screens/PuzzleScreen.tsx
+++ src/screens/PuzzleScreen.tsx
@@ -9,8 +9,8 @@ import type { Puzzle } from '../types'
 const compactKeyRows = [
   ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
   ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
-  ['Z', 'X', 'C', 'V', 'B', 'N', 'M'],
-  ['SPACE', 'BACKSPACE'],
+  ['Z', 'X', 'C', 'V', 'B', 'N', 'M', 'BACKSPACE'],
+  ['SPACE'],
 ]
 
 function CompactAnswerKeyboard({ onKey }: { onKey: (key: string) => void }) {
